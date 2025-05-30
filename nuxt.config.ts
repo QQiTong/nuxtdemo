80c94de7b4f89302bdee92fe433c3ae752df44d7
unknown <qqitong@163.com>
2025-05-30 17:31:50 +0800

    hydry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt', '@nuxt/image'],
+  routeRules: {
+    "/spa": { ssr: false },
+    "/static": { static: true },
+    "/swr": { swr: true },
+  },
 })
